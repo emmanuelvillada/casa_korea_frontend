d7314573179677b5f76530e68a49c430e2f1d6d1
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2026-02-17 15:59:05 -0500

    updating images host

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["w7.pngwing.com", "cdn.sanity.io"], // Agrega el dominio de la imagen externa
+    domains: ["cdn.sanity.io"], // Agrega el dominio de la imagen externa
   },
 };
 
